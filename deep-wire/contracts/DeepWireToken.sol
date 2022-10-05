67f9244b447087aa6dcf2bd642cabbcda539337c
Brandon Behbahani <brandonbehbahani@gmail.com>
2022-10-05 10:49:30 -0400

    Adding Stuff
    Just getting started creating a sketleton app.

---
+++ deep-wire/contracts/DeepWireToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+import {ERC20} from './ERC20.sol'; 
+
+contract DeepWireToken is ERC20 {
+
+    function  post() public {
+
+    }
+
+
+}
